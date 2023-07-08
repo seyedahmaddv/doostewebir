117802214f3e005e4cc246fd34ef890546642df7
Seyed Ahmad <110930355+seyedahmaddv@users.noreply.github.com>
2023-07-08 14:54:30 +0330

    Create getStaticProps01.js

---
+++ getStaticProps01.js
@@ -0,0 +1,12 @@
+export default function Home(props) { ... }
+
+export async function getStaticProps() {
+  // Get external data from the file system, API, DB, etc.
+  const data = ...
+
+  // The value of the `props` key will be
+  //  passed to the `Home` component
+  return {
+    props: ...
+  }
+}
